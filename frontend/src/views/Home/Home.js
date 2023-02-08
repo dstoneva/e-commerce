54ee46175a471a9a1cabae93961fc0a67f6b4ac3
dstoneva <stoilova.denitsa@gmail.com>
2023-02-08 13:43:14 +0200

    Add flash deals slider to homepage

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -1,12 +1,13 @@
 import { Grid } from '@mui/material'
 import { PageLayout } from 'layouts/Main/components'
-import { Carousel, NewArrivals, TopCategories, TopRatings, FeaturedBrands, ProductList, Delivery } from './components'
+import { Carousel, NewArrivals, TopCategories, TopRatings, FeaturedBrands, ProductList, Delivery, FlashDeals } from './components'
 
 const Home = () => {
   return (
     <>
       <Carousel />
       <PageLayout container isAsync={false}>
+        <FlashDeals/>
         <TopCategories />
         <Grid container spacing={4} sx={{ my: 1 }}>
           <Grid item md={6} xs={12}>
