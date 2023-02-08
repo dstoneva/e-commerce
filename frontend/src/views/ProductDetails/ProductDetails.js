813649730635f7359c8acf68d6e768eef7f743bf
dstoneva <stoilova.denitsa@gmail.com>
2023-02-08 20:40:35 +0200

    Create product details card

--- frontend/src/views/ProductDetails/ProductDetails.js
+++ frontend/src/views/ProductDetails/ProductDetails.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Container } from '@mui/material'
+import { PageLayout } from 'layouts/Main/components'
+import { useParams } from 'react-router-dom'
+import useSWR from 'swr'
+import { ProductDetailsCard } from './components'
 
 const ProductDetails = () => {
+  const params = useParams()
+  const { data: product, error } = useSWR(`/products/${params.productId}`)
+
   return (
-    <div>ProductDetails</div>
+    <PageLayout error={error} data={product}>
+      <Container
+        sx={{
+          mt: 5,
+        }}
+      >
+        <ProductDetailsCard product={product} />
+      </Container>
+    </PageLayout>
   )
 }
 
---
+++ frontend/src/views/ProductDetails/components/ProductDetailsCard/ProductDetailsCard.js
@@ -0,0 +1,130 @@
+import { Typography, Box, CardMedia, Grid, Rating, Button } from '@mui/material'
+import { DisplayCurrency } from 'components'
+import { useState } from 'react'
+import { useCart } from 'core'
+import AddIcon from '@mui/icons-material/Add'
+import RemoveIcon from '@mui/icons-material/Remove'
+import theme from 'theme'
+
+const ProductDetailsCard = ({ product }) => {
+  const { getFinalPrice, addToCart, removeFromCart, itemIds, getQuantity } = useCart()
+
+  const [mainImage, setMainImage] = useState(0)
+  const changeImageHandler = (event, index) => {
+    setMainImage(index)
+  }
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={6}>
+        <Box display="flex" alignItems="center" flexDirection="column">
+          <CardMedia
+            loading="eager"
+            component="img"
+            src={product.images[mainImage]}
+            sx={{ width: 300, height: 300, mb: 5, borderRadius: 2 }}
+          />
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'row',
+              gap: 3,
+              overflow: 'auto',
+            }}
+          >
+            {product.images.slice(0, 3).map((image, i) => (
+              <Box
+                key={i}
+                onClick={(event) => changeImageHandler(event, i)}
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  p: 2,
+                  bgcolor: 'background.paper',
+                  border: `1px solid ${mainImage === i ? theme.palette.primary.main : theme.palette.grey[400]}`,
+                  '&:hover': {
+                    cursor: 'pointer',
+                  },
+                  borderRadius: 2,
+                  width: 64,
+                  height: 64,
+                  minWidth: 64,
+                }}
+              >
+                <Box display="flex" component="img" src={image} />
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Box display="flex" flexDirection="column" gap={2} justifyContent="center">
+          <Typography variant="h4" fontWeight="bold">
+            {product.title}
+          </Typography>
+          <Typography>
+            Brand: <b>{product.brand}</b>
+          </Typography>
+          <Box display="flex" gap={1} alignItems="center">
+            <Typography variant="subtitle">Rated:</Typography>
+            <Rating value={product.rating} readOnly size="small" />
+            <Typography variant="subtitle">(50)</Typography>
+          </Box>
+          <Typography fontWeight="bold" color="primary" fontSize={25}>
+            <DisplayCurrency number={getFinalPrice(product)} />
+          </Typography>
+          <Typography variant="subtitle2">{product.stock > 0 ? 'Stock available' : 'Out of Stock'}</Typography>
+          <Box display="flex" gap={3}>
+            {itemIds.includes(product._id) ? (
+              <>
+                <Button
+                  color="primary"
+                  onClick={() => removeFromCart(product._id)}
+                  variant="outlined"
+                  sx={{ minWidth: 0, p: '3px' }}
+                >
+                  <RemoveIcon fontSize="small" />
+                </Button>
+                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                  <Typography textAlign="center" fontWeight={600} fontSize={20}>
+                    {getQuantity(product._id)}
+                  </Typography>
+                </Box>
+                <Button
+                  color="primary"
+                  variant="outlined"
+                  sx={{ minWidth: 0, p: '3px' }}
+                  onClick={() => addToCart(product)}
+                >
+                  <AddIcon fontSize="small" />
+                </Button>
+              </>
+            ) : (
+              <Button
+                disabled={product.stock === 0 ? true : false}
+                onClick={() => addToCart(product)}
+                variant="contained"
+                color="primary"
+                sx={{
+                  color: 'white',
+                  width: 150,
+                  ':hover': {
+                    bgcolor: 'primary.dark',
+                  },
+                }}
+              >
+                {product.stock > 0 ? 'Add to cart' : 'Out of stock'}
+              </Button>
+            )}
+          </Box>
+          <Typography sx={{ mt: 2 }}>
+            Sold by: <b>Some store</b>
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default ProductDetailsCard
