88a9a676832c109aeb286c3db325a3348d844a06
dstoneva <stoilova.denitsa@gmail.com>
2023-02-13 22:45:17 +0200

    Create search drawer for mobile view

---
+++ frontend/src/layouts/Main/components/NavigationBar/components/SearchButton/SearchButton.js
@@ -0,0 +1,49 @@
+import { Close } from '@mui/icons-material'
+import SearchIcon from '@mui/icons-material/Search'
+import { Avatar, Box, Drawer, IconButton, Typography } from '@mui/material'
+import { useState } from 'react'
+import LiveSearch from '../LiveSearch'
+
+const SearchButton = () => {
+  const [state, setState] = useState(false)
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return
+    }
+
+    setState(open)
+  }
+
+  return (
+    <Box>
+      <IconButton sx={{ p: 0 }} onClick={toggleDrawer(true)}>
+        <Avatar>
+          <SearchIcon />
+        </Avatar>
+      </IconButton>
+
+      <Drawer
+        anchor="top"
+        open={state}
+        onClose={toggleDrawer(false)}
+        PaperProps={{
+          sx: { display: 'flex', flexDirection: 'column', justifyContent: 'space-between' },
+        }}
+      >
+        <Box sx={{ p: 2 }}>
+          <Typography variant="subtitle">Search Bazaar</Typography>
+          <IconButton size="small" sx={{ position: 'absolute', top: 8, right: 8 }} onClick={toggleDrawer(false)}>
+            <Close />
+          </IconButton>
+        </Box>
+
+        <Box sx={{ height: '100vh', padding: 2, width: 'auto' }}>
+          <LiveSearch toggleDrawer={toggleDrawer} setState={setState} />
+        </Box>
+      </Drawer>
+    </Box>
+  )
+}
+
+export default SearchButton
