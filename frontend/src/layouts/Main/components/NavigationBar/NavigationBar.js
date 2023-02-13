fa21bbad7c155c1394e8e4b0065e653376515e65
dstoneva <stoilova.denitsa@gmail.com>
2023-02-13 17:38:05 +0200

    Combine component imports

--- frontend/src/layouts/Main/components/NavigationBar/NavigationBar.js
+++ frontend/src/layouts/Main/components/NavigationBar/NavigationBar.js
@@ -1,13 +1,15 @@
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
-import Menu from '@mui/material/Menu'
-import Container from '@mui/material/Container'
-import Avatar from '@mui/material/Avatar'
-import Tooltip from '@mui/material/Tooltip'
-import MenuItem from '@mui/material/MenuItem'
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Avatar,
+  Tooltip,
+  MenuItem,
+} from '@mui/material'
 import { useState } from 'react'
 import { useAuth } from 'core'
 import { Link } from 'react-router-dom'
