f381a770f4b984b92765507d1af70d2fc87ae530
dstoneva <stoilova.denitsa@gmail.com>
2023-02-15 12:38:33 +0200

    Fix empty cart view

--- frontend/src/views/Cart/Cart.js
+++ frontend/src/views/Cart/Cart.js
@@ -1,4 +1,4 @@
-import { Grid, useMediaQuery } from '@mui/material'
+import { Grid, useMediaQuery, Typography, Box, Link } from '@mui/material'
 import { useCart } from 'core'
 import { PageLayout } from 'layouts/Main/components'
 import { CartProduct } from './components'
@@ -11,20 +11,32 @@ const Cart = () => {
   const { cart } = useCart()
   return (
     <PageLayout container isAsync={false}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={8} lg={8}>
-          {cart.map((product) =>
-            !isMobile ? (
-              <CartProduct key={product._id} product={product} />
-            ) : (
-              <ProductCard key={product._id} product={product} />
-            )
-          )}
+      {cart.length < 1 ? (
+        <Box display="flex" alignItems="center" justifyContent='center' flexDirection="column" gap={2} height={450}>
+          <Box component="img" src="/images/cart.png" alt="Cart" height={200} />
+          <Typography align="center" variant="h4">
+            Your cart is currently empty!
+          </Typography>
+          <Typography variant="subtitle1" color="gray" align="center">
+            Looks like you have not made your choice yet. Browse our awesome store, <Link href='/'>start shopping now</Link>!
+          </Typography>
+        </Box>
+      ) : (
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={8} lg={8}>
+            {cart.map((product) =>
+              !isMobile ? (
+                <CartProduct key={product._id} product={product} />
+              ) : (
+                <ProductCard key={product._id} product={product} />
+              )
+            )}
+          </Grid>
+          <Grid item xs={12} md={4} lg={4}>
+            {location.pathname === '/cart' ? <SideMenu /> : <Subtotal />}
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={4} lg={4}>
-          {location.pathname === '/cart' ? <SideMenu /> : <Subtotal />}
-        </Grid>
-      </Grid>
+      )}
     </PageLayout>
   )
 }
