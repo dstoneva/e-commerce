12029201d18c5a0c671292ac01ab064642d59ca9
Denitsa Toneva <stoilova.denitsa@gmail.com>
2024-11-22 17:53:47 +0200

    Refactor Description component to add loading skeleton for product details

--- frontend/src/views/ProductDetails/components/AdditionalInfo/components/Description/Description.js
+++ frontend/src/views/ProductDetails/components/AdditionalInfo/components/Description/Description.js
@@ -1,16 +1,26 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Skeleton } from '@mui/material'
 
-const Description = ({ product }) => {
+const Description = ({ product, isLoading }) => {
   return (
     <Box>
       <Typography fontSize={22} fontWeight={600} sx={{ my: 3 }}>
         Specification:
       </Typography>
-      <Typography fontSize={15}>Brand: {product.brand}</Typography>
-      <Typography fontSize={15}>Model: {product.title}</Typography>
-      <Typography fontSize={15}>{product.description}</Typography>
-      <Typography fontSize={15}>Features: FM Radio, Card Supported (Micro SD / TF)</Typography>
-      <Typography fontSize={15}>Made in China</Typography>
+
+      {isLoading ? (
+        <>
+          <Skeleton variant="text" width="60%" height={24} />
+          <Skeleton variant="text" width="70%" height={24} />
+          <Skeleton variant="text" width="90%" height={24} />
+          <Skeleton variant="text" width="50%" height={24} />
+        </>
+      ) : (
+        <>
+          {product.brand && <Typography fontSize={15}>Brand: {product.brand}</Typography>}
+          <Typography fontSize={15}>Model: {product.title}</Typography>
+          <Typography fontSize={15}>{product.description}</Typography>
+        </>
+      )}
     </Box>
   )
 }
