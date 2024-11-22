c6ea791dcee8af307d7694bcc8f9a32d97084e18
Denitsa Toneva <stoilova.denitsa@gmail.com>
2024-11-22 18:40:38 +0200

    Refactor Home component to add banner slides and update layout

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material'
+import { Container, Grid } from '@mui/material'
 import { PageLayout } from 'layouts/Main/components'
 import {
   Carousel,
@@ -10,13 +10,53 @@ import {
   Delivery,
   FlashDeals,
   ScrollToTopButton,
+  BannerSlide,
 } from './components'
+
+const bannerSlides = [
+  <BannerSlide
+    title="50% Off For Your First Shopping"
+    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis."
+    buttonText="Shop Now"
+    imageUrl="https://bazaar.ui-lib.com/assets/images/products/nike-black.png"
+    altText="Nike black"
+    onButtonClick={() => console.log('Navigating to shop now')}
+    imageProps={{
+      sx: {
+        height: { xs: 320, sm: 'auto' },
+        maxHeight: { xs: 320, sm: 350, md: 375, lg: 400 },
+        width: { xs: '100%' },
+        objectFit: 'contain',
+      },
+    }}
+  />,
+  <BannerSlide
+    title="Exclusive Winter Sale - Up to 70% Off!"
+    description="Explore our exclusive deals and discounts for the summer season. Don’t miss out on great prices!"
+    imageUrl="/images/winter-sale.webp"
+    altText="Winter Sale"
+    imageProps={{
+      sx: {
+        height: { xs: 320, sm: 'auto' },
+        width: { xs: '100%' },
+        objectFit: 'contain',
+      },
+    }}
+  />,
+]
 
 const Home = () => {
   return (
     <>
-      <Carousel />
-      <PageLayout container isAsync={false}>
+      <Carousel
+        slides={bannerSlides}
+        pagination={{ clickable: true, type: 'bullets' }}
+        loop={true}
+        speed={800}
+        containerStyles={{ backgroundColor: 'background.white' }}
+        slideStyles={{ my: 2 }}
+      />{' '}
+      <Container>
         <FlashDeals />
         <TopCategories />
         <Grid container spacing={4} sx={{ my: 1 }}>
@@ -37,7 +77,7 @@ const Home = () => {
           </Grid>
         </Grid>
         <ScrollToTopButton />
-      </PageLayout>
+      </Container>
     </>
   )
 }
