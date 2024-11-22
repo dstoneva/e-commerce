84673af897b77e27e2537a899c017d88a55cef11
Denitsa Toneva <stoilova.denitsa@gmail.com>
2024-11-22 15:24:09 +0200

    Refactor Logo component to fix image path

--- frontend/src/components/Logo/Logo.js
+++ frontend/src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material"
 const Logo = () => {
-  return <Box component='img' src='images/bazaar-black-sm.svg' height={60} width={60}/>
+  return <Box component='img' src='/images/bazaar-black-sm.svg' height={60} width={60}/>
 }
 
 export default Logo
