9a682869e77ca74725dcced852ee6f95c08c6b3c
Denitsa Toneva <stoilova.denitsa@gmail.com>
2024-11-26 22:50:46 +0200

    Refactor ProductList component to improve loading state handling and simplify code

--- frontend/src/views/Home/components/ProductList/ProductList.js
+++ frontend/src/views/Home/components/ProductList/ProductList.js
@@ -5,38 +5,39 @@ import { useState } from 'react'
 import { ProductCard } from 'components'
 import Headline from '../Headline'
 import WidgetsIcon from '@mui/icons-material/Widgets'
-import ResourceView from 'components/ResourceView/ResourceView'
 import ProductCardSkeleton from 'components/ProductCard/ProductCardSkeleton'
 
 const ProductList = ({ pageNum, productsNum, headline, pagination }) => {
   const [page, setPage] = useState(pageNum)
   const [productsPerPage, setProductsPerPage] = useState(productsNum)
 
-  const { data: products, error, isLoading } = useSWR(`/products?page=${page}&productsPerPage=${productsPerPage}`)
+  const {
+    data: products,
+    error,
+    isValidating,
+  } = useSWR(`/products?page=${page}&productsPerPage=${productsPerPage}`, { dedupingInterval: 60000 })
+
+  const isLoading = !products && isValidating
 
   return (
     <PageLayout error={error} data={products}>
       <Headline icon={<WidgetsIcon color="primary" />} sx={{ marginTop: 4 }}>
         {headline}
       </Headline>
-      
-      <ResourceView isLoading={isLoading} loadingComponent={
-        <Grid container spacing={3}>
-          {Array.from({ length: productsPerPage }).map((_, index) => (
-            <Grid item xl={3} lg={3} md={4} sm={6} xs={12} key={index}>
-              <ProductCardSkeleton />
-            </Grid>
-          ))}
-        </Grid>
-      }>
-        <Grid container spacing={3}>
-          {products?.result.map((product) => (
-            <Grid item xl={3} lg={3} md={4} sm={6} xs={12} key={product._id}>
-              <ProductCard quickView product={product} />
-            </Grid>
-          ))}
-        </Grid>
-      </ResourceView>
+
+      <Grid container spacing={3}>
+        {isLoading
+          ? Array.from({ length: productsPerPage }).map((_, index) => (
+              <Grid item xl={3} lg={3} md={4} sm={6} xs={12} key={index}>
+                <ProductCardSkeleton />
+              </Grid>
+            ))
+          : products?.result.map((product) => (
+              <Grid item xl={3} lg={3} md={4} sm={6} xs={12} key={product._id}>
+                <ProductCard quickView product={product} />
+              </Grid>
+            ))}
+      </Grid>
 
       {pagination && (
         <Box display="flex" alignItems="center" justifyContent="space-between" mt={4}>
