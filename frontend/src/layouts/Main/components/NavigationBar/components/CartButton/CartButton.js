716bb7a396db4d8a6ed6c597f02b0de7535d33dd
Denitsa Toneva <stoilova.denitsa@gmail.com>
2024-11-26 22:48:35 +0200

    Refactor CartButton component to remove unused code and dependencies, and update cart display in the drawer

--- frontend/src/layouts/Main/components/NavigationBar/components/CartButton/CartButton.js
+++ frontend/src/layouts/Main/components/NavigationBar/components/CartButton/CartButton.js
@@ -1,27 +1,13 @@
 import { Close, ShoppingBag } from '@mui/icons-material'
-import {
-  Avatar,
-  Badge,
-  Box,
-  Button,
-  Card,
-  CardContent,
-  CardMedia,
-  Divider,
-  Drawer,
-  IconButton,
-  Typography,
-} from '@mui/material'
-import { DisplayCurrency } from 'components'
+import { Avatar, Badge, Box, Button, Divider, Drawer, IconButton, Typography } from '@mui/material'
+import { DisplayCurrency, ProductCard } from 'components'
 import { useCart } from 'core'
 import { Fragment, useState, useCallback } from 'react'
-import RemoveIcon from '@mui/icons-material/Remove'
-import AddIcon from '@mui/icons-material/Add'
 import { useNavigate } from 'react-router-dom'
 import { PageURLs } from 'Routes'
 
 const CartButton = () => {
-  const { cart, removeFromCart, addToCart, getFinalPrice, totalPrice, quantity, resetCart } = useCart()
+  const { cart, totalPrice, quantity, resetCart } = useCart()
   const navigate = useNavigate()
   const [isOpen, setIsOpen] = useState(false)
 
@@ -30,20 +16,6 @@ const CartButton = () => {
     if (event?.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) return
     setIsOpen(false)
   }, [])
-
-  const handleRemove = useCallback(
-    (productId) => {
-      removeFromCart(productId)
-    },
-    [removeFromCart]
-  )
-
-  const handleAdd = useCallback(
-    (product) => {
-      addToCart(product)
-    },
-    [addToCart]
-  )
 
   return (
     <div>
@@ -71,70 +43,26 @@ const CartButton = () => {
         <IconButton size="small" sx={{ position: 'absolute', top: 8, right: 8 }} onClick={closeDrawer}>
           <Close />
         </IconButton>
-        <Box>
-          <Box display="flex" alignItems="center" p={2}>
-            <ShoppingBag color="secondary" />
-            <Typography variant="body1" color="secondary" sx={{ ml: 1 }}>
-              {quantity} {quantity < 2 ? 'item' : 'items'}
-            </Typography>
-          </Box>
-          <Divider />
-          {cart.map((product) => {
-            const finalPrice = getFinalPrice(product)
-            return (
-              <Fragment key={product._id}>
-                <Card sx={{ p: 2, display: 'flex', height: 100, alignItems: 'center' }} elevation={0}>
-                  <CardMedia component="img" sx={{ width: 100 }} image={product.thumbnail} alt={product.title} />
-                  <CardContent sx={{ flex: '1 0 auto', p: 0, pl: 2, pb: '0 !important' }}>
-                    <Box display="flex" justifyContent="space-between">
-                      <Box maxWidth="140px">
-                        <Typography variant="body1" noWrap>
-                          {product.title}
-                        </Typography>
-                        <Typography variant="overline" component="div">
-                          {product.discountPercentage > 0 ? (
-                            <Box display="flex" alignItems="center">
-                              <Box>
-                                <DisplayCurrency number={finalPrice} />
-                              </Box>
-                              <Box
-                                sx={{
-                                  textDecoration: 'line-through',
-                                  ml: 1,
-                                  color: 'error.main',
-                                  display: 'inline',
-                                }}
-                              >
-                                <DisplayCurrency number={product.price} />
-                              </Box>
-                            </Box>
-                          ) : (
-                            <DisplayCurrency number={finalPrice} />
-                          )}
-                        </Typography>
-                      </Box>
-                      <Box display="flex" alignItems="center">
-                        <IconButton onClick={() => handleRemove(product._id)} size="small">
-                          <RemoveIcon />
-                        </IconButton>
-                        <Typography variant="body1">{product.quantity}</Typography>
-                        <IconButton
-                          onClick={() => handleAdd(product)}
-                          disabled={product.quantity >= product.stock}
-                          size="small"
-                        >
-                          <AddIcon />
-                        </IconButton>
-                      </Box>
-                    </Box>
-                  </CardContent>
-                </Card>
-                <Divider />
-              </Fragment>
-            )
-          })}
+        <Box display="flex" alignItems="center" height={60} p={2}>
+          <ShoppingBag color="secondary" />
+          <Typography variant="body1" color="secondary" sx={{ ml: 1 }}>
+            {quantity} {quantity < 2 ? 'item' : 'items'}
+          </Typography>
         </Box>
-        <Box p={2}>
+        <Divider />
+        <Box
+          sx={{
+            overflowY: 'auto',
+            height: `calc(100% - 60px - 155px)`,
+          }}
+        >
+          {cart.map((product) => (
+            <Fragment key={product._id}>
+              <ProductCard product={product} inCartDrawer />
+            </Fragment>
+          ))}
+        </Box>
+        <Box height={155} p={2}>
           <Button
             disabled={quantity === 0}
             color="primary"
