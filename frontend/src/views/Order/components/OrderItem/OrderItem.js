3f7d4bb9b5b8a82c2b21357fcdf5378a53b2f8e6
Denitsa Toneva <stoilova.denitsa@gmail.com>
2024-12-04 01:01:11 +0200

    Refactor OrderItem component to include a "Write a Review" button with a link to the product page

--- frontend/src/views/Order/components/OrderItem/OrderItem.js
+++ frontend/src/views/Order/components/OrderItem/OrderItem.js
@@ -1,10 +1,12 @@
 import { Box, Button, Typography } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 import { DisplayCurrency } from 'components'
 import { useCart } from 'core'
 
 const OrderItem = ({ product }) => {
   const { getFinalPrice } = useCart()
   const finalPrice = getFinalPrice(product?.product)
+  const navigate = useNavigate()
 
   return (
     <Box
@@ -17,7 +19,7 @@ const OrderItem = ({ product }) => {
         flexWrap: 'wrap',
       }}
     >
-      <Box display="flex" alignItems="center" overflow='hidden'>
+      <Box display="flex" alignItems="center" overflow="hidden">
         <Box
           component="img"
           sx={{ height: 90, width: 90, borderRadius: 4, mr: 2 }}
@@ -25,14 +27,17 @@ const OrderItem = ({ product }) => {
           alt={product.product?.title}
         />
         <Box sx={{ display: 'flex', flexDirection: 'column', fontWeight: '500', width: 200 }}>
-          <Typography noWrap variant="body">{product?.product?.title}</Typography>
+          <Typography noWrap variant="body">
+            {product?.product?.title}
+          </Typography>
           <Typography variant="overline">
             <DisplayCurrency number={finalPrice} /> x {product?.quantity}
           </Typography>
         </Box>
       </Box>
-      <Typography>Product properties:</Typography>
-      <Button color="primary">Write a Review</Button>
+      <Button onClick={() => navigate(`/products/${product.product?._id}`)} color="primary">
+        Write a Review
+      </Button>
     </Box>
   )
 }
