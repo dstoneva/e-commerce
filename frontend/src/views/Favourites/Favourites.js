098b26b265419d5970a439caa329690697f7a7be
Denitsa Toneva <stoilova.denitsa@gmail.com>
2024-12-04 00:59:49 +0200

    Refactor Favourites component to improve loading behavior and empty state display

--- frontend/src/views/Favourites/Favourites.js
+++ frontend/src/views/Favourites/Favourites.js
@@ -5,60 +5,57 @@ import { PageLayout } from 'layouts/Main/components'
 import { EmptyState, ProductCard } from 'components'
 import { Headline } from 'views/Home/components'
 import FavoriteIcon from '@mui/icons-material/Favorite'
-import ResourceView from 'components/ResourceView/ResourceView'
 import ProductCardSkeleton from 'components/ProductCard/ProductCardSkeleton'
 import { PageURLs } from 'Routes'
 
 const Favourites = () => {
-  const { favourites, isFavouritesLoading, error } = useFavourites()
+  const { favourites, error } = useFavourites()
   const navigate = useNavigate()
 
   return (
-    <PageLayout container isAsync={false}>
-      <ResourceView
-        isLoading={isFavouritesLoading}
-        isError={!!error}
-        errorMessage="Failed to load your favorites. Please try again."
-        loadingComponent={
+    <PageLayout
+      container
+      error={error}
+      data={favourites}
+      loading={
+        <Grid container spacing={3}>
+          {Array.from({ length: 4 }).map((_, index) => (
+            <Grid item xl={3} lg={3} md={4} sm={6} xs={12} key={index}>
+              <ProductCardSkeleton />
+            </Grid>
+          ))}
+        </Grid>
+      }
+    >
+      {favourites.length < 1 ? (
+        <EmptyState
+          image="/images/wishlist-svgrepo-com.svg"
+          altText="Empty Wishlist"
+          title="Your wishlist is currently empty!"
+          subtitle="Looks like you have not made your choice yet. Browse our awesome store and add items to your wishlist!"
+          primaryAction={{
+            text: 'Start Shopping',
+            onClick: () => navigate('/'),
+          }}
+          secondaryAction={{
+            text: 'Discover Favourites',
+            onClick: () => navigate(PageURLs.Favourites),
+          }}
+        />
+      ) : (
+        <>
+          <Headline sx={{ mb: 4, ml: 0.5 }} icon={<FavoriteIcon color="primary" />}>
+            Favorites
+          </Headline>
           <Grid container spacing={3}>
-            {Array.from({ length: 4 }).map((_, index) => (
-              <Grid item xl={3} lg={3} md={4} sm={6} xs={12} key={index}>
-                <ProductCardSkeleton />
+            {favourites.map((product) => (
+              <Grid item xl={3} lg={3} md={4} sm={6} xs={12} key={product._id}>
+                <ProductCard quickView product={product} />
               </Grid>
             ))}
           </Grid>
-        }
-      >
-        {favourites.length < 1 ? (
-          <EmptyState
-            image="/images/wishlist-svgrepo-com.svg"
-            altText="Empty Wishlist"
-            title="Your wishlist is currently empty!"
-            subtitle="Looks like you have not made your choice yet. Browse our awesome store and add items to your wishlist!"
-            primaryAction={{
-              text: 'Start Shopping',
-              onClick: () => navigate('/'),
-            }}
-            secondaryAction={{
-              text: 'Discover Favourites',
-              onClick: () => navigate(PageURLs.Favourites),
-            }}
-          />
-        ) : (
-          <>
-            <Headline sx={{ mb: 4, ml: 0.5 }} icon={<FavoriteIcon color="primary" />}>
-              Favorites
-            </Headline>
-            <Grid container spacing={3}>
-              {favourites.map((product) => (
-                <Grid item xl={3} lg={3} md={4} sm={6} xs={12} key={product._id}>
-                  <ProductCard quickView product={product} />
-                </Grid>
-              ))}
-            </Grid>
-          </>
-        )}
-      </ResourceView>
+        </>
+      )}
     </PageLayout>
   )
 }
