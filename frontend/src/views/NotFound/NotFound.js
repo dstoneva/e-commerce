cee21172a700a4d231cfe44e0ae42a6223f88ebf
Denitsa Toneva <stoilova.denitsa@gmail.com>
2024-12-21 17:25:55 +0200

    Refactor NotFound component to improve layout and add navigation buttons

--- frontend/src/views/NotFound/NotFound.js
+++ frontend/src/views/NotFound/NotFound.js
@@ -11,19 +11,27 @@ const NotFound = () => {
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        minHeight: 245,
-        my: 5,
+        minHeight: '100dvh',
+        p: 5,
       }}
     >
-      <Box component="img" src="/images/404.svg" sx={{ width: '40%', maxHeight: '50vh' }} />
+      <Box
+        component="img"
+        src="https://template.getbazaar.io/_next/static/media/404.ee3d5ebc.svg"
+        sx={{ maxWidth: 320 }}
+      />
       <Box
         sx={{
           display: 'flex',
           justifyContent: 'space-between',
           alignItems: 'center',
           my: 5,
+          gap: 2,
         }}
       >
+        <Button variant="contained" color="primary" onClick={() => navigate(-1)} sx={{ width: 116 }}>
+          Go Back
+        </Button>
         <Button variant="outlined" color="primary" onClick={() => navigate('/')} sx={{ width: 116 }}>
           Home
         </Button>
